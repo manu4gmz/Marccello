59f72b27023dc559e54057cd9d65d0ebf8c879cb
Manuel <manuel4gmz@gmail.com>
2020-03-04 14:10:56 -0300

    Login & register form 10/10

--- front/src/components/Input.jsx
+++ front/src/components/Input.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Style from "./Input.css";
 
-export default ({value, placeholder}) => {
+export default ({value, placeholder, onChange, search, name }) => {
   return (
     <div className="inputContainer">
-      <img src="assets/search.svg"/>
-      <input placeholder={placeholder} type="text" className="inputClean" value={value}/>
+      { search ? <img src="assets/search.svg"/> : null }
+      <input onChange={onChange || null} name={name} placeholder={placeholder} type="text" className="inputClean" value={value}/>
     </div>
   );
 };
