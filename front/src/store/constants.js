7b490da04ba4e90a27139beebb4119533be13816
NachoRodVil <ignaciorodvil@gmail.com>
2020-03-09 14:14:38 -0300

    estan las reviews

--- front/src/store/constants.js
+++ front/src/store/constants.js
@@ -9,3 +9,7 @@ export const SET_USER = 'SET_USER';
 export const USER_LOGIN = 'USER_LOGIN';
 
 export const USER_LOGOUT = 'USER_LOGOUT';
+
+export const SET_REVIEWS = "SET_REVIEWS";
+
+export const CREATE_REVIEW = "CREATE_REVIEW";
--- front/src/store/reducers/index.js
+++ front/src/store/reducers/index.js
@@ -3,10 +3,12 @@ import productReducer from "./productReducer";
 import loginReducer from "./loginReducer";
 import cartReducer from "./cartReducer";
 import userReducer from './userReducer';
+import reviewsReducers from "./reviewsReducers";
 
 export default combineReducers({
   products: productReducer,
   login: loginReducer,
   cart: cartReducer,
   user: userReducer,
+  reviews: reviewsReducers
 });
