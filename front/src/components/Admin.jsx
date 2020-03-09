bda6e4356a0bb5157c1f25635962a16381852d5d
Ramiro Aragone <ramiro@10.100.0.32>
2020-03-09 19:46:36 -0300

    editar producto incompleto

--- front/src/components/Admin.jsx
+++ front/src/components/Admin.jsx
@@ -5,12 +5,14 @@ import Orders from "../containers/Orders";
 import SingleOrder from "../containers/SingleOrder";
 import NewProductContainer from "../containers/NewProductContainer";
 import AdminProductListContainer from "../containers/AdminProductListContainer";
+import EditProductContainer from "../containers/EditProductContainer";
 
 export default ({ match }) => {
   return (
     <Fragment>
       <Switch>
         <Route path={match.path + "/create-product"} exact component={NewProductContainer} />
+        <Route path={match.path + "/edit-product/:id"} component={EditProductContainer} />
         <Route path={match.path + "/edit-product"} exact component={AdminProductListContainer} />
         <Route path={match.path + "/orders/:id"} exact component={SingleOrder} />
         <Route path={match.path + "/orders"} exact component={Orders} />
--- front/src/containers/EditProductContainer.jsx
+++ front/src/containers/EditProductContainer.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import NewProduct from '../components/NewProduct'
-import { createProduct } from '../store/actions/products'
+import { createProduct, fetchProduct } from '../store/actions/products'
 import { connect } from 'react-redux'
-
+import EditProduct from '../components/EditProduct'
 
 const mapDispatchToProps = (dispatch, ownProps) => {
     return {
-        createProduct: (product) => dispatch(createProduct(product)),
+        fetchProduct: (product) => dispatch(fetchProduct(product))
     }
 }
 
-class NewProductContainer extends React.Component {
+const mapStateToProps = function (state, ownProps) {
+    return {
+        product: state.products.product,
+        state
+    };
+};
+
+class EditProductContainer extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
@@ -18,14 +25,33 @@ class NewProductContainer extends React.Component {
             inputImage: '',
             inputSock: '',
             inputPrice: '',
-            inputVisible: 'false' ? false : true,
+            inputVisible: '',
             descriptionInput: ''
         }
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
     }
 
+    componentDidMount() {
+        const id = this.props.match.params.id
+        this.props.fetchProduct(id)
+            .then(data => data.product)
+            .then(data => this.setState({
+
+                inputName: data.name,
+                inputImage: data.imgURL,
+                inputSock: data.stock,
+                inputPrice: data.price,
+                inputVisible: data.visible,
+                descriptionInput: data.description
+            })
+            )
+
+    }
+
     handleChange(e) {
+
+        console.log('ESTOS SON LOS INPUTS', { [e.target.name]: e.target.value })
         this.setState({ [e.target.name]: e.target.value })
     }
 
@@ -40,21 +66,20 @@ class NewProductContainer extends React.Component {
             visible: this.state.inputVisible
         })
 
+
+
     }
 
     render() {
-
+        console.log('ESTAS SON LAS PROPSSSSSS', this.props)
         return (
-            <NewProduct
+            <EditProduct
                 handleChange={this.handleChange}
                 handleSubmit={this.handleSubmit}
+                state={this.state}
             />
         )
     }
 }
 
-export default connect(null, mapDispatchToProps)(NewProductContainer)
-
-
-
-
+export default connect(mapStateToProps, mapDispatchToProps)(EditProductContainer)
