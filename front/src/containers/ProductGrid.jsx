f44f3fe9dd9b58dc5c18896acf29956df60f2c35
Manuel <manuel4gmz@gmail.com>
2020-03-12 17:46:35 -0300

    Mejore el seed

--- front/src/containers/ProductGrid.jsx
+++ front/src/containers/ProductGrid.jsx
@@ -188,7 +188,7 @@ class ProductGrid extends React.Component {
             <div style={{ padding: "4% 0" }}></div>
           </Row>
 
-          <Row>
+          <Row style={{ minHeight: "80vh"}}>
             {products.map((product, i) => (
               <ProductModule
                 alreadyCart={cart.map(p => p.id).includes(product.id)}
