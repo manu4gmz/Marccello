8855e9e04593562fa7687a72e544b54fd28374cc
Ramiro Aragone <ramiro@10.100.0.32>
2020-03-12 15:00:20 -0300

    bugfix visible true

--- front/src/containers/EditProductContainer.jsx
+++ front/src/containers/EditProductContainer.jsx
@@ -5,83 +5,83 @@ import { connect } from "react-redux";
 import EditProduct from "../components/EditProduct";
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-  return {
-    fetchProduct: product => dispatch(fetchProduct(product)),
-    editProduct: (productid, product) =>
-      dispatch(editProduct(productid, product))
-  };
+    return {
+        fetchProduct: product => dispatch(fetchProduct(product)),
+        editProduct: (productid, product) =>
+            dispatch(editProduct(productid, product))
+    };
 };
 
-const mapStateToProps = function(state, ownProps) {
-  return {
-    product: state.products.product,
-    state
-  };
+const mapStateToProps = function (state, ownProps) {
+    return {
+        product: state.products.product,
+        state
+    };
 };
 
 class EditProductContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputName: "",
-      inputImage: "",
-      inputStock: "",
-      inputPrice: "",
-      inputVisible: "",
-      descriptionInput: ""
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputName: "",
+            inputImage: "",
+            inputStock: "",
+            inputPrice: "",
+            inputVisible: false || 'false' ? false : true,
+            descriptionInput: ""
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-  handleSubmit(e) {
-    e.preventDefault();
-    this.setState({ [e.target.name]: e.target.value });
-    this.props
-      .editProduct(this.props.product.id, {
-        name: this.state.inputName,
-        price: Number(this.state.inputPrice),
-        description: this.state.descriptionInput,
-        stock: Number(this.state.inputStock),
-        imgURL: this.state.inputImage,
-        visible: this.state.inputVisible
-      })
-      .then(() => this.props.history.push("/admin/edit-product"));
-  }
+    handleSubmit(e) {
+        e.preventDefault();
+        this.setState({ [e.target.name]: e.target.value });
+        this.props
+            .editProduct(this.props.product.id, {
+                name: this.state.inputName,
+                price: Number(this.state.inputPrice),
+                description: this.state.descriptionInput,
+                stock: Number(this.state.inputStock),
+                imgURL: this.state.inputImage,
+                visible: this.state.inputVisible
+            })
+            .then(() => this.props.history.push("/admin/edit-product"));
+    }
 
-  componentDidMount() {
-    const id = this.props.match.params.id;
-    this.props
-      .fetchProduct(id)
-      .then(data => data.product)
-      .then(data =>
-        this.setState({
-          inputName: data.name,
-          inputImage: data.imgURL,
-          inputStock: data.stock,
-          inputPrice: data.price,
-          inputVisible: data.visible,
-          descriptionInput: data.description
-        })
-      );
-  }
+    componentDidMount() {
+        const id = this.props.match.params.id;
+        this.props
+            .fetchProduct(id)
+            .then(data => data.product)
+            .then(data =>
+                this.setState({
+                    inputName: data.name,
+                    inputImage: data.imgURL,
+                    inputStock: data.stock,
+                    inputPrice: data.price,
+                    inputVisible: data.visible,
+                    descriptionInput: data.description
+                })
+            );
+    }
 
-  handleChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-  }
+    handleChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
 
-  render() {
-    return (
-      <EditProduct
-        handleChange={this.handleChange}
-        handleSubmit={this.handleSubmit}
-        state={this.state}
-      />
-    );
-  }
+    render() {
+        return (
+            <EditProduct
+                handleChange={this.handleChange}
+                handleSubmit={this.handleSubmit}
+                state={this.state}
+            />
+        );
+    }
 }
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(EditProductContainer);
